63fd0d163efae4d55db78e31af7b94be5f25b475
zyl <zhangyanlong@megalinkware.com>
2023-04-05 23:51:05 +0800

    day61

---
+++ src/main/java/other/L1025.java
@@ -0,0 +1,14 @@
+package other;
+
+/**
+ * @author: zyl
+ * @description: 除数博弈
+ */
+public class L1025 {
+
+    class Solution1025 {
+        public boolean divisorGame(int n) {
+            return n%2 == 0;
+        }
+    }
+}
---
+++ src/main/java/string/L1160.java
@@ -0,0 +1,12 @@
+package string;
+
+/**
+ * @author: zyl
+ * @description: 拼写单词
+ */
+public class L1160 {
+
+    public int countCharacters(String[] words, String chars) {
+        return 1;
+    }
+}
