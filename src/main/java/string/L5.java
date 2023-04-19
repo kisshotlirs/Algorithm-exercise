a4c22d2cacc0076c009d23827bf2d7dcda595650
zyl <zhangyanlong@megalinkware.com>
2023-04-19 21:42:17 +0800

    day72

--- src/main/java/string/L5.java
+++ src/main/java/string/L5.java
@@ -9,18 +9,64 @@ package string;
  */
 public class L5 {
     public static void main(String[] args) {
-
+        String s = "cbbd";
+        System.out.println(s.substring(0, 4));
     }
 }
 
 class Solution2 {
     public String longestPalindrome(String s) {
-        int l = s.length()-1;
-        for (int i = 0; i < l; i++) {
-            for (int j=i+1; j<l; j++){
+        int len = s.length();
+        if (len < 2){
+            return s;
+        }
 
+        int maxLen = 1;
+        int begin = 0;
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] chars = s.toCharArray();
+        for (int L=2; L<=len; L++){
+            //枚举左边界
+            for (int i = 0; i < len; i++) {
+                //确定右边界
+                int j=L+i-1;
+                if (j >= len){
+                    break;
+                }
+                if (chars[i] != chars[j]){
+                    dp[i][j] = false;
+                } else {
+                    if (j-i < 3){
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+
+                if (dp[i][j] && j-i+1>maxLen){
+                    maxLen = j-i+1;
+                    begin = i;
+                }
             }
         }
-        return "";
+        return s.substring(begin,begin+maxLen);
     }
+
+    public boolean isRound(String s){
+        int len = s.length();
+        if (len <= 1){
+            return false;
+        }
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) != s.charAt(len-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
