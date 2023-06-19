c751b6f36761109f8ea78b6e2b43b8848b54bb81
zyl <3523915495@qq.com>
2023-06-19 20:29:06 +0800

    day103

---
+++ src/main/java/math/L495.java
@@ -0,0 +1,22 @@
+package math;
+
+/**
+ * @author: zyl
+ * @description: 提莫攻击
+ */
+public class L495 {
+
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int ans = 0;
+        int expired = 0;
+        for (int i = 0; i < timeSeries.length; ++i) {
+            if (timeSeries[i] >= expired) {
+                ans += duration;
+            } else {
+                ans += timeSeries[i] + duration - expired;
+            }
+            expired = timeSeries[i] + duration;
+        }
+        return ans;
+    }
+}
